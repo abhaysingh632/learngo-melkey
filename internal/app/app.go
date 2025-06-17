d9627107baefd22e055f1b8dcc2ffbf67b6fbd9b
Abhay Singh <abhay1029singh@gmail.com>
2025-06-17 12:53:36 +0530

    inital commit

---
+++ internal/app/app.go
@@ -0,0 +1,36 @@
+package app
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/melkeydev/femProject/internal/api"
+)
+
+type Application struct {
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
+}
+
+func NewApplication() (*Application, error) {
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// our stores will go here
+
+	// our handlers will go here
+	workoutHandler := api.NewWorkoutHandler()
+
+	app := &Application{
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
+	}
+
+	return app, nil
+}
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
+}
